a72dc64e8d7bd1491910fd8d377e757b314c1f2d
gabrieldocs <mydocs.lucas@gmail.com>
2020-03-21 11:32:40 -0300

    Ramo painel, configurando esquemas de cores

--- microlearn/settings.py
+++ microlearn/settings.py
@@ -51,6 +51,42 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+JET_THEMES = [
+    {
+        'theme': 'default', # theme folder name
+        'color': '#47bac1', # color of the theme's button in user menu
+        'title': 'Default' # theme title
+    },
+    {
+        'theme': 'green',
+        'color': '#44b78b',
+        'title': 'Green'
+    },
+    {
+        'theme': 'light-green',
+        'color': '#2faa60',
+        'title': 'Light Green'
+    },
+    {
+        'theme': 'light-violet',
+        'color': '#a464c4',
+        'title': 'Light Violet'
+    },
+    {
+        'theme': 'light-blue',
+        'color': '#5EADDE',
+        'title': 'Light Blue'
+    },
+    {
+        'theme': 'light-gray',
+        'color': '#222',
+        'title': 'Light Gray'
+    }
+]
+
+JET_DEFAULT_THEME = 'light-green'
+
+
 ROOT_URLCONF = 'microlearn.urls'
 
 TEMPLATES = [
